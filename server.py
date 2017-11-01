19b05414e8f3a051a43a47bd64b114b0dfd06617
varnitgoel <17356399+varnitgoel@users.noreply.github.com>
2017-11-01 14:53:25 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -5,3 +5,22 @@ Created on Wed Nov  1 14:43:30 2017
 @author: Varnit Goel
 """
 #testing 1
+import socket
+
+#creating socket object 
+s=socket.socket()
+host=socket.gethostname()
+print(host)
+port=12221
+
+s.bind((host,port))
+
+s.listen(7)
+
+while TRUE:
+    c,adrs=s.accept()
+    print("got connceted from %s", +str(adrs))
+    
+    response="welcome to the chatroom"
+    c.send(response.encode('ascii'))
+    c.close() 
