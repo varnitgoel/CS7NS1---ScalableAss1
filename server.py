10c2d1b1ff2fb60e051385fe9a7c9a81e69b202c
varnitgoel <17356399+varnitgoel@users.noreply.github.com>
2017-11-01 18:08:40 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -24,3 +24,32 @@ while TRUE:
     response="welcome to the chatroom"
     c.send(response.encode('ascii'))
     c.close() 
+    
+
+
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 50000
+server.bind((host,port))
+print(host)
+thread_count = []
+
+g1_clients = []
+g2_clients = []
+
+
+while True:
+        server.listen(4)
+        (csock,(ip,port)) = server.accept()
+
+        print("Connected to ",port,ip)
+        #monitoring connections
+
+        clThread = client_threads(ip,port,csock)
+        clThread.start()
+        thread_count.append(clThread)
+        print("Threads :")
+        print(thread_count)
+        print(g1_clients)
