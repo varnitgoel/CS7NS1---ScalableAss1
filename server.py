e798481e9038445f6ed34428a14267d7ddcf640d
varnitgoel <17356399+varnitgoel@users.noreply.github.com>
2017-11-02 01:04:34 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -4,52 +4,31 @@ Created on Wed Nov  1 14:43:30 2017
 
 @author: Varnit Goel
 """
-#testing 1
-import socket
+class client_threads(Thread):
 
-#creating socket object 
-s=socket.socket()
-host=socket.gethostname()
-print(host)
-port=12221
+	def __init__(self,ip,port,socket):
+		Thread.__init__(self)
+		self.ip = ip
+		self.port = port
+		self.chatroom =[] 
+		self.socket = socket
+		self.uid = random.randint(1000,2000)
+		self.roomname = ''
+		self.clientname = ''
 
-s.bind((host,port))
-
-s.listen(7)
-
-while TRUE:
-    c,adrs=s.accept()
-    print("got connceted from %s", +str(adrs))
-    
-    response="welcome to the chatroom"
-    c.send(response.encode('ascii'))
-    c.close() 
-    
-
-
-
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
-port = 50000
-server.bind((host,port))
-print(host)
-thread_count = []
-
-g1_clients = []
-g2_clients = []
-
-
-while True:
-        server.listen(4)
-        (csock,(ip,port)) = server.accept()
-
-        print("Connected to ",port,ip)
-        #monitoring connections
-
-        clThread = client_threads(ip,port,csock)
-        clThread.start()
-        thread_count.append(clThread)
-        print("Threads :")
-        print(thread_count)
-        print(g1_clients)
+	def run(self):
+		while True:
+			conn_msg = csock.recv(1024)
+			cflag = check_msg(conn_msg)
+			print('Connected')
+			if cflag == 1 :
+				 print('joining')
+				 self.roomname,self.clientname = join(conn_msg,csock)
+			elif cflag == 2 : leave(conn_msg,csock)
+			elif cflag == 3 : discon(csock)
+			elif cflag == 4 : chat()
+			else : pass					 #error code for incorrect message	
+			print(self.clientname)
+			self.chatroom.append(self.roomname)
+			print('roomnames')
+			print(self.chatroom)
